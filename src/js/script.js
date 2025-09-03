8e73f9121f3ae38b68b21bf3881e2acbaf48c15d
juliapompu <juliapompeusouza200@gmail.com>
2025-09-03 17:44:29 -0300

    fix/pesquisa, alinhamento dos produtos na pag principal

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
-
 function filtrarCards(categoria) {
     const cards = document.querySelectorAll('.Nossos-produtos .card');
     cards.forEach(card => {
         if (categoria === 'todos' || card.getAttribute('data-categoria') === categoria) {
-            card.style.display = 'block';
+            card.style.display = 'flex';
         } else {
             card.style.display = 'none';
         }
